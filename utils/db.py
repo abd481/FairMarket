30cb0a56a9800c220a6dc057302393b8d750de8c
Abdelrahman Sayed <abdelrahmansayd198@gmail.com>
2026-08-13 09:00:54 +0300

    feat(db): add lazy shared postgres engine

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,9 @@
+from sqlalchemy import create_engine
 from pymongo import MongoClient
 from utils.secrets import get_secret
 
 _client = None
+_pg_engine = None
 
 
 def get_mongo_client() -> MongoClient:
@@ -18,3 +20,11 @@ def get_database(db_name: str = "real_estate_db"):
 
 def get_collection(collection_name: str, db_name: str = "real_estate_db"):
     return get_database(db_name)[collection_name]
+
+
+def get_pg_engine():
+    """Lazily create (and cache) the PostgreSQL SQLAlchemy engine."""
+    global _pg_engine
+    if _pg_engine is None:
+        _pg_engine = create_engine(get_secret("POSTGRES", "postgres"))
+    return _pg_engine
